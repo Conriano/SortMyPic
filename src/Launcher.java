a246843d0b7591c9de87e38579fba1f310719624
Julian Einspenner <julianeinspenner@yahoo.de>
2018-01-27 11:38:44 +0100

    Gives information about the working time now

--- src/Launcher.java
+++ src/Launcher.java
@@ -5,7 +5,8 @@ public class Launcher {
 	 * @param args = command line parameters, means your file and your aim 
 	 */
 	public static void main(String[] args) {
-			
+			long time1 = System.currentTimeMillis();
+		
 			if(args.length != 2) {
 				System.err.println("Usage: java sortmypic imagePath.xxx imgDestinyPath.xxx");
 				System.exit(1);
@@ -13,5 +14,9 @@ public class Launcher {
 			
 			Start s = new Start(args);
 			s.work();
+			
+			long time2 = System.currentTimeMillis() -time1;
+			
+			System.out.println("Time needed: " + time2 + " ms");
 	}
 }
